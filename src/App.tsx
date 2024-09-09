c7d076a6aa26676991583452105929a5c5ed06b0
Vactor0911 <vactor0911@gmail.com>
2024-09-09 13:21:56 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Style = styled.div`
+`;
+
+function App() {
+  return (
+    <Style>
+      <h1>뭐먹지?</h1>
+      <p>What to eat?</p>
+    </Style>
+  )
+}
+
+export default App
